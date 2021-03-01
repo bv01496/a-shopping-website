fb7a3f35dde511bc7f615201c1ddcdc4be9714e6
bv01496 <bv01496@gmail.com>
2021-03-01 21:54:59 +0530

    heroku deploy

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -127,6 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
