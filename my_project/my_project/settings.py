75173ac3fc761ac508a929e749aa173aa553144a
bv01496 <bv01496@gmail.com>
2021-03-02 10:25:02 +0530

    heroku deploy

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '%v$tqhnbctdg)y0om0i8qv1s-#tkk&r_ijzl*+74g$yeq1h(xw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleWare',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- my_project/my_project/urls.py
+++ my_project/my_project/urls.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
 from django.urls import path,include
+from django.views.static import serve
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('home.urls')),
-    path('',include('products.urls'))
+    path('',include('products.urls')),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
