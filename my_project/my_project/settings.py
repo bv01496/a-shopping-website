029492dcf13d6a8aeaacaf89f5876c86cee2b79d
bv01496 <bv01496@gmail.com>
2021-03-02 10:38:47 +0530

    added domin

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '%v$tqhnbctdg)y0om0i8qv1s-#tkk&r_ijzl*+74g$yeq1h(xw'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1','struds.herokuapp.com']
 
 
 # Application definition
